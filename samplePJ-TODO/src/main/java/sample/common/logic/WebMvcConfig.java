22dfdb385195ab3a4df72ac93de330d1172d787a
tomonariY <y.tomonari1011@gmail.com>
2026-07-03 17:43:18 +0900

    08 修正完了

--- samplePJ-TODO/src/main/java/sample/common/logic/WebMvcConfig.java
+++ samplePJ-TODO/src/main/java/sample/common/logic/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package sample.common.logic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
-	@Autowired
-	private LoginInterceptor loginInterceptor;
+	private final LoginInterceptor loginInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
