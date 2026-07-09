71735e1205f79af47d4b22e969e8f0fd00fc787a
tomonariY <y.tomonari1011@gmail.com>
2026-07-09 18:24:59 +0900

    15修正完了

---
+++ samplePJ-TODO/src/main/java/sample/common/logic/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package sample.common.logic;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(BusinessException.class)
+	public String handleBusiness(BusinessException e, RedirectAttributes ra) {
+		ra.addFlashAttribute("error", e.getMessage());
+		return "redirect:/tasks";
+	}
+}
