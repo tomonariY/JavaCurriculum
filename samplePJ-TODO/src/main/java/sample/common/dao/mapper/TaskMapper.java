77f712a7bbef624d8a01c6ca6dac7c3621cbe16e
tomonariY <y.tomonari1011@gmail.com>
2026-07-23 17:46:51 +0900

    TaskMapperにCSVエクスポート用のSQLを追加

--- samplePJ-TODO/src/main/java/sample/common/dao/mapper/TaskMapper.java
+++ samplePJ-TODO/src/main/java/sample/common/dao/mapper/TaskMapper.java
@@ -1,5 +1,6 @@
 package sample.common.dao.mapper;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -9,21 +10,31 @@ import sample.common.dao.entity.Task;
 
 @Mapper
 public interface TaskMapper {
-	
-		
+
 	// 新規作成のため
 	void insertTask(Task task);
-		
+
 	// ページネーションのため
 	List<Task> selectPageByUser(@Param("username") String username,
-								@Param("limit") int limit,
-								@Param("offset") int offset);
+			@Param("limit") int limit,
+			@Param("offset") int offset);
+
 	long countTotalByUser(@Param("username") String username);
+
 	// id取得のため
 	Task findByIdAndUser(@Param("id") Long id, @Param("username") String username);
+
 	// 更新のため
 	int updateTaskByUser(Task task);
+
 	// 削除のため
 	int deleteTaskByUser(@Param("id") Long id, @Param("username") String username);
 
+	// CSV出力のため
+	List<Task> selectTasksForExportByPeriod(@Param("username") String username,
+			@Param("startDate") LocalDate startDate,
+			@Param("endDate") LocalDate endDate);
+
+	Task selectTaskForExportById(@Param("username") String username,
+			@Param("id") Long id);
 }
